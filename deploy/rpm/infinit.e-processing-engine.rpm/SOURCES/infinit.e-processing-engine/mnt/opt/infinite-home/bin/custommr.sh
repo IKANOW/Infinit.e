9bc8ed16d04790a4d97c94dccabbf67fd07c684c
apiggott <apiggott@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-05-11 12:51:31 +0000

    INF-1464 #comment Fixed typo in custommr script #time 15m
    git-svn-id: https://ikanow.jira.com/svn/INF/branches/infinit.e-published-to-github@5025 e6edf6fb-f266-4316-afb4-e53d95876a76

--- deploy/rpm/infinit.e-processing-engine.rpm/SOURCES/infinit.e-processing-engine/mnt/opt/infinite-home/bin/custommr.sh
+++ deploy/rpm/infinit.e-processing-engine.rpm/SOURCES/infinit.e-processing-engine/mnt/opt/infinite-home/bin/custommr.sh
@@ -3,6 +3,6 @@ runningNameNode=`pgrep -f namenode.NameNode`
 #check if custom is already running
 runningCustom=`pgrep -f 'infinit.e.core.server.*.jar --custom'`
 #if no process id's returned for namenode and custom, run job
-if [ -z $runningNameNode ] && [-z $runningCustom]; then
+if [ -z "$runningNameNode" ] && [-z "$runningCustom" ]; then
         java -jar /mnt/opt/infinite-home/lib/infinit.e.core.server.jar --custom
 fi
