c4cbc551f9699060f34b5a31d03470b2a4b3be77
apiggott <apiggott@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-05-15 14:30:41 +0000

    INF-1400 #comment Just moved harvest extractors into data model and changed their licenses to Apache #time 10m
    git-svn-id: https://ikanow.jira.com/svn/INF/branches/infinit.e-published-to-github@5046 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ core/infinit.e.data_model/src/com/ikanow/infinit/e/data_model/interfaces/harvest/IEntityExtractor.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright 2012 The Infinit.e Open Source Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.ikanow.infinit.e.data_model.interfaces.harvest;
+
+import com.ikanow.infinit.e.data_model.InfiniteEnums.ExtractorDailyLimitExceededException;
+import com.ikanow.infinit.e.data_model.InfiniteEnums.ExtractorDocumentLevelException;
+import com.ikanow.infinit.e.data_model.store.document.DocumentPojo;
+
+public interface IEntityExtractor 
+{
+	String getName();
+	
+	/**
+	 * Takes a feed with some of the information stored in it
+	 * such as title, desc, etc, and needs to parse the full
+	 * text and add entities, events, and other metadata.
+	 * 
+	 * 
+	 * @param partialDoc The DocumentPojo before extraction with fulltext field to extract on
+	 * @return The DocumentPojo after extraction with entities, events, and full metadata
+	 */
+	void extractEntities(DocumentPojo partialDoc) 
+		throws ExtractorDailyLimitExceededException, ExtractorDocumentLevelException;
+	
+	/**
+	 * Simliar to extractEntities except this case assumes that
+	 * text extraction has not been done and therefore takes the
+	 * url and extracts the full text and entities/events.
+	 * 
+	 * @param partialDoc The DocumentPojo before text extraction (empty fulltext field)
+	 * @return The DocumentPojo after text extraction and entity/event extraction with fulltext, entities, events, etc
+	 */
+	void extractEntitiesAndText(DocumentPojo partialDoc)
+		throws ExtractorDailyLimitExceededException, ExtractorDocumentLevelException;
+	
+	/**
+	 * Attempts to lookup if this extractor has a given capability,
+	 * if it does returns value, otherwise null
+	 * 
+	 * @param capability Extractor capability we are looking for
+	 * @return Value of capability, or null if capability not found
+	 */
+	String getCapability(EntityExtractorEnum capability);
+}
---
+++ core/infinit.e.data_model/src/com/ikanow/infinit/e/data_model/interfaces/harvest/ITextExtractor.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright 2012 The Infinit.e Open Source Project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.ikanow.infinit.e.data_model.interfaces.harvest;
+
+import com.ikanow.infinit.e.data_model.InfiniteEnums.ExtractorDailyLimitExceededException;
+import com.ikanow.infinit.e.data_model.InfiniteEnums.ExtractorDocumentLevelException;
+import com.ikanow.infinit.e.data_model.store.document.DocumentPojo;
+
+public interface ITextExtractor 
+{
+	String getName();
+	
+	/**
+	 * Takes a url and spits back the text of the
+	 * site, usually cleans it up some too.
+	 * 
+	 * @param partialDoc The document whose fulltext we want to populate from its url
+	 * @return The success/error status
+	 */
+	void extractText(DocumentPojo partialDoc)
+		throws ExtractorDailyLimitExceededException, ExtractorDocumentLevelException;
+}
