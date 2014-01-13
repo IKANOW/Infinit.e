45b685e92f024b1cbc2b51b8a98d8e4757a22ff1
Alex Piggott <apiggott@ikanow.com>
2014-01-13 14:58:24 -0500

    Jan 2014 patch: fix typo in script

--- deploy/rpm/infinit.e-processing-engine.rpm/SOURCES/infinit.e-processing-engine/mnt/opt/infinite-home/bin/harvest_redistribute.sh
+++ deploy/rpm/infinit.e-processing-engine.rpm/SOURCES/infinit.e-processing-engine/mnt/opt/infinite-home/bin/harvest_redistribute.sh
@@ -4,5 +4,5 @@ MASTER=`curl -s http://localhost:9200/_cluster/state | grep -o "master_node.:.[^
 IS_MASTER=$(curl -s http://localhost:9200/_cluster/nodes/_local | grep -q "$MASTER" && echo "true")
 
 if echo $IS_MASTER  | grep -qi "true"; then
-	/usr/bin/mongo --quiet ingest --eval "var isMidNoon = $1" $SCRIPTDIR/google_harvest_distribution_script.js
+	/usr/bin/mongo --quiet ingest --eval "var isMidNoon = $1" $SCRIPTDIR/harvest_redistribution_script.js
 fi
